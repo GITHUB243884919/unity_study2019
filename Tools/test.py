b98b0d116fd1d9abcb90a7b5602611b379ab33ad
243884919 <243884919@QQ.COM>
2019-02-09 16:48:58 +0800

    增加excele导出工具

---
+++ Tools/test.py
@@ -0,0 +1,8 @@
+import xlrd
+
+workbook = xlrd.open_workbook('Config.xlsx')
+for booksheet in workbook.sheets():
+    for col in range(booksheet.ncols):
+        for row in range(booksheet.nrows):
+            value = booksheet.cell(row, col).value
+            print (value)
