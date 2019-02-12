fd7753603e8c89a4f00739cc99852a173b1b5e43
243884919 <243884919@QQ.COM>
2019-02-12 11:21:42 +0800

    配置工具

--- Assets/Scripts/luaconfig/api/Sheet1API.cs
+++ Assets/Scripts/luaconfig/api/Sheet1API.cs
@@ -22,6 +22,17 @@ namespace GameName.Lua.Config
 			}
 			return null;
 		}
+        
+		public static Sheet1 GetDataBy_id (int target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			foreach (var item in cacheList){
+				if (item.id == target) {
+					return item;
+				}
+			}
+			return null;
+		}
 
 		public static List<Sheet1> GetDataListBy_id (List<Sheet1> list, int target)
 		{
@@ -34,9 +45,32 @@ namespace GameName.Lua.Config
 			return res;
 		}
 
+		public static List<Sheet1> GetDataListBy_id (int target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			List<Sheet1> res = new List<Sheet1> ();
+			foreach (var item in cacheList){
+				if (item.id == target) {
+					res.Add (item);
+				}
+			}
+			return res;
+		}
+
 		public static Sheet1 GetDataBy_field1 (List<Sheet1> list, string target)
 		{
 			foreach (var item in list){
+				if (item.field1 == target) {
+					return item;
+				}
+			}
+			return null;
+		}
+        
+		public static Sheet1 GetDataBy_field1 (string target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			foreach (var item in cacheList){
 				if (item.field1 == target) {
 					return item;
 				}
@@ -55,9 +89,32 @@ namespace GameName.Lua.Config
 			return res;
 		}
 
+		public static List<Sheet1> GetDataListBy_field1 (string target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			List<Sheet1> res = new List<Sheet1> ();
+			foreach (var item in cacheList){
+				if (item.field1 == target) {
+					res.Add (item);
+				}
+			}
+			return res;
+		}
+
 		public static Sheet1 GetDataBy_field2 (List<Sheet1> list, int target)
 		{
 			foreach (var item in list){
+				if (item.field2 == target) {
+					return item;
+				}
+			}
+			return null;
+		}
+        
+		public static Sheet1 GetDataBy_field2 (int target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			foreach (var item in cacheList){
 				if (item.field2 == target) {
 					return item;
 				}
@@ -76,9 +133,32 @@ namespace GameName.Lua.Config
 			return res;
 		}
 
+		public static List<Sheet1> GetDataListBy_field2 (int target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			List<Sheet1> res = new List<Sheet1> ();
+			foreach (var item in cacheList){
+				if (item.field2 == target) {
+					res.Add (item);
+				}
+			}
+			return res;
+		}
+
 		public static Sheet1 GetDataBy_field4 (List<Sheet1> list, float target)
 		{
 			foreach (var item in list){
+				if (item.field4 == target) {
+					return item;
+				}
+			}
+			return null;
+		}
+        
+		public static Sheet1 GetDataBy_field4 (float target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			foreach (var item in cacheList){
 				if (item.field4 == target) {
 					return item;
 				}
@@ -97,9 +177,32 @@ namespace GameName.Lua.Config
 			return res;
 		}
 
+		public static List<Sheet1> GetDataListBy_field4 (float target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			List<Sheet1> res = new List<Sheet1> ();
+			foreach (var item in cacheList){
+				if (item.field4 == target) {
+					res.Add (item);
+				}
+			}
+			return res;
+		}
+
 		public static Sheet1 GetDataBy_field5 (List<Sheet1> list, double target)
 		{
 			foreach (var item in list){
+				if (item.field5 == target) {
+					return item;
+				}
+			}
+			return null;
+		}
+        
+		public static Sheet1 GetDataBy_field5 (double target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			foreach (var item in cacheList){
 				if (item.field5 == target) {
 					return item;
 				}
@@ -111,6 +214,18 @@ namespace GameName.Lua.Config
 		{
 			List<Sheet1> res = new List<Sheet1> ();
 			foreach (var item in list){
+				if (item.field5 == target) {
+					res.Add (item);
+				}
+			}
+			return res;
+		}
+
+		public static List<Sheet1> GetDataListBy_field5 (double target)
+		{
+			var cacheList = LuaConfigManager.Instance.sheet1Parse.CacheList;
+			List<Sheet1> res = new List<Sheet1> ();
+			foreach (var item in cacheList){
 				if (item.field5 == target) {
 					res.Add (item);
 				}
--- Assets/Scripts/luaconfig/api/TEST_Sheet1API.cs
+++ Assets/Scripts/luaconfig/api/TEST_Sheet1API.cs
@@ -22,6 +22,17 @@ namespace GameName.Lua.Config
 			}
 			return null;
 		}
+        
+		public static TEST_Sheet1 GetDataBy_id (int target)
+		{
+			var cacheList = LuaConfigManager.Instance.tEST_Sheet1Parse.CacheList;
+			foreach (var item in cacheList){
+				if (item.id == target) {
+					return item;
+				}
+			}
+			return null;
+		}
 
 		public static List<TEST_Sheet1> GetDataListBy_id (List<TEST_Sheet1> list, int target)
 		{
@@ -34,9 +45,32 @@ namespace GameName.Lua.Config
 			return res;
 		}
 
+		public static List<TEST_Sheet1> GetDataListBy_id (int target)
+		{
+			var cacheList = LuaConfigManager.Instance.tEST_Sheet1Parse.CacheList;
+			List<TEST_Sheet1> res = new List<TEST_Sheet1> ();
+			foreach (var item in cacheList){
+				if (item.id == target) {
+					res.Add (item);
+				}
+			}
+			return res;
+		}
+
 		public static TEST_Sheet1 GetDataBy_field1 (List<TEST_Sheet1> list, string target)
 		{
 			foreach (var item in list){
+				if (item.field1 == target) {
+					return item;
+				}
+			}
+			return null;
+		}
+        
+		public static TEST_Sheet1 GetDataBy_field1 (string target)
+		{
+			var cacheList = LuaConfigManager.Instance.tEST_Sheet1Parse.CacheList;
+			foreach (var item in cacheList){
 				if (item.field1 == target) {
 					return item;
 				}
@@ -55,9 +89,32 @@ namespace GameName.Lua.Config
 			return res;
 		}
 
+		public static List<TEST_Sheet1> GetDataListBy_field1 (string target)
+		{
+			var cacheList = LuaConfigManager.Instance.tEST_Sheet1Parse.CacheList;
+			List<TEST_Sheet1> res = new List<TEST_Sheet1> ();
+			foreach (var item in cacheList){
+				if (item.field1 == target) {
+					res.Add (item);
+				}
+			}
+			return res;
+		}
+
 		public static TEST_Sheet1 GetDataBy_field2 (List<TEST_Sheet1> list, int target)
 		{
 			foreach (var item in list){
+				if (item.field2 == target) {
+					return item;
+				}
+			}
+			return null;
+		}
+        
+		public static TEST_Sheet1 GetDataBy_field2 (int target)
+		{
+			var cacheList = LuaConfigManager.Instance.tEST_Sheet1Parse.CacheList;
+			foreach (var item in cacheList){
 				if (item.field2 == target) {
 					return item;
 				}
@@ -69,6 +126,18 @@ namespace GameName.Lua.Config
 		{
 			List<TEST_Sheet1> res = new List<TEST_Sheet1> ();
 			foreach (var item in list){
+				if (item.field2 == target) {
+					res.Add (item);
+				}
+			}
+			return res;
+		}
+
+		public static List<TEST_Sheet1> GetDataListBy_field2 (int target)
+		{
+			var cacheList = LuaConfigManager.Instance.tEST_Sheet1Parse.CacheList;
+			List<TEST_Sheet1> res = new List<TEST_Sheet1> ();
+			foreach (var item in cacheList){
 				if (item.field2 == target) {
 					res.Add (item);
 				}
