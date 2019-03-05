f0df1870ad914f203139c87b5106948127c0910a
243884919 <243884919@QQ.COM>
2019-03-05 20:26:57 +0800

    大更新

---
+++ Assets/Scripts/UFrame/ResourceManagement/Editor/AddScenesToBuildSetting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UFrame;
+public class AddScenesToBuildSetting
+{
+    [MenuItem("UFrame框架/资源管理/开发模式/添加场景")]
+    static void AddAllScenes()
+    {
+        List<string> scenesPath = new List<string>();
+
+        string scenesPathRoot = "Assets/" + UFrameConst.Scene_Dir;
+        foreach (var path in Directory.GetFiles(scenesPathRoot, "*.unity", SearchOption.AllDirectories))
+        {
+            scenesPath.Add(path.Replace("\\", "/"));
+        }
+
+        EditorBuildSettingsScene[] newSettings = new EditorBuildSettingsScene[scenesPath.Count];
+        for (int i = 0; i < newSettings.Length; i++)
+        {
+            newSettings[i] = new EditorBuildSettingsScene(scenesPath[i], true);
+        }
+        EditorBuildSettings.scenes = newSettings;
+        AssetDatabase.Refresh();
+        AssetDatabase.SaveAssets();
+    }
+}
