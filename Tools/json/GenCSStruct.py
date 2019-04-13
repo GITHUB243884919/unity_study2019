80df929f9beb5fc242299bec0c504ec2b093565b
243884919 <243884919@QQ.COM>
2019-04-13 14:59:10 +0800

    json

--- Tools/json/GenCSStruct.py
+++ Tools/json/GenCSStruct.py
@@ -44,7 +44,8 @@ def Gen(dir, sheetName, name_row, desc_row, type_row, col_num, nameSpace, struct
     for i in range(0, col_num):
         var_name = str(name_row[i])
         var_type = str(type_row[i])
-
+        var_desc = str(desc_row[i])
+        
         if var_name.startswith("*"):
             continue;
         if len(var_name) <= 0:
@@ -65,12 +66,12 @@ def Gen(dir, sheetName, name_row, desc_row, type_row, col_num, nameSpace, struct
         elif (var_type == "array"):
             fieldText += fieldContext % (desc_row[i], "ArrayData", var_name)
         elif (var_type == "INT"):
-            fieldText += fieldContext % (desc_row[i].replace("\n", "\n        /// "), "int", var_name)
+            fieldText += fieldContext % (var_desc.replace("\n", "\n        /// "), "int", var_name)
         elif (var_type == "STRING"):
-            fieldText += fieldContext % (desc_row[i].replace("\n", "\n        /// "), "string", var_name)
+            fieldText += fieldContext % (var_desc.replace("\n", "\n        /// "), "string", var_name)
         elif (var_type == "FLOAT"):
             #print(sheetName + " " + desc_row[i])
-            fieldText += fieldContext % (desc_row[i].replace("\n", "\n        /// "), "string", var_name)
+            fieldText += fieldContext % (var_desc.replace("\n", "\n        /// "), "float", var_name)
 
     writeContext = fileContext % (nameSpace, className, fieldText, toStringText)
     
